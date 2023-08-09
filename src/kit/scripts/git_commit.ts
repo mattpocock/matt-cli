99583800ad7f6ff42f9bbd899f15f0bed50f84f6
Matt Pocock <mattpocockvoice@gmail.com>
2023-08-09 09:31:57 +0100

    2023-08-09

--- src/kit/scripts/git_commit.ts
+++ src/kit/scripts/git_commit.ts
@@ -2,6 +2,8 @@ const run = async () => {
   // current date in YYYY-MM-DD format
   const currentDate = new Date().toISOString().split("T")[0];
 
+  $.verbose = true;
+
   await $`git add -A && git commit -m "${currentDate}" && git push`;
 };
 
